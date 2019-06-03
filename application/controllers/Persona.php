3e25344fa3392fa4cf030e7cd3597b1b74068038
Rojas <hernanrojasutani@gmail.com>
2019-06-03 17:01:54 -0500

    Add Codeigniter Framework

---
+++ application/controllers/Persona.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Persona extends CI_Controller {
+
+	public function index(){
+		echo 'hola';
+	}
+
+}
+
+/* End of file Persona.php */
+/* Location: ./application/controllers/Persona.php */
